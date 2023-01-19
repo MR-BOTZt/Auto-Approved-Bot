6ec57dc9af10c545e5b896078ca2e608496cd412
Yo babe <104895694+Jeolpaul2@users.noreply.github.com>
2023-01-19 21:11:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,14 +37,24 @@ pr0fess0r_99=Client(
 
 CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).split()]
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
-APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
+
 
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
 async def start(client: pr0fess0r_99, message: Message):
-    approvedbot = await client.get_me() 
-    button = [[ InlineKeyboardButton("📦 Repo", url="https://github.com/PR0FESS0R-99/Auto-Approved-Bot"), InlineKeyboardButton("Updates 📢", url="t.me/Mo_Tech_YT") ],
-              [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
-    await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+    bot = await client.get_me()
+    await message.reply_text(
+        text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot"
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("📢 BOT UPDATES", url="https://t.me/REQUSET_ACCEPT_BOT"),
+            InlineKeyboardButton("📢 BOT SUPPORT", url="https://t.me/REQUSET_ACCEPT_BOT")
+            ],[            
+            InlineKeyboardButton("➕️ ADD ME GROUP", url=f"https://t.me/{bot.username}?startgroup=true"),
+            InlineKeyboardButton("➕️ ADD ME CHANNEL", url=f"https://t.me/{bot.username}?startchannel=true") 
+            ]]
+            )
+        )
+
+#    await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart)**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
 @pr0fess0r_99.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
@@ -52,9 +62,12 @@ async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     user=message.from_user # User
     print(f"{user.first_name} Joined 🤝") # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
-        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
-    #   print("Welcome....")
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton("📢 BOT UPDATES", url="https://t.me/REQUSET_ACCEPT_BOT")]])
+    await client.send_message(
+         user.id,
+            f"**Hello {message.from_user.mention}!\nYou Request To Join {message.chat.title} Was Approved.🏻",
+            reply_markup=btn
+        )
 
-print("Auto Approved Bot")
+print("Bot Started. Editz By @jeol_tg")
 pr0fess0r_99.run()
